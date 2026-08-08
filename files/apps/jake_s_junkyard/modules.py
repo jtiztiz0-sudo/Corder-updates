cf1517438d2f763d66c8c2738c0335b298671963
jtiztiz0-sudo <jtiztiz0@gmail.com>
2026-08-07 19:32:52 -0500

    Update to version 20

--- files/apps/jake_s_junkyard/modules.py
+++ files/apps/jake_s_junkyard/modules.py
@@ -27,7 +27,7 @@ BUSINESS = {'name': "Jackson's a Poopy",
  'tagline': 'take reservations, keep up with staff hours, and track what we owe '
             'suppliers',
  'theme': {},
- 'generated': '2026-08-07 19:19'}
+ 'generated': '2026-08-07 19:32'}
 
 MODULES = [{'key': 'scheduling',
   'label': 'Schedule',
--- files/apps/t_town_closet/modules.py
+++ files/apps/t_town_closet/modules.py
@@ -27,7 +27,7 @@ BUSINESS = {'name': 'T-Town Closet',
  'tagline': 'it needs to track once a rental is placed and IF it requires shipping. '
             'automatically make a shipping label  the cheapest option',
  'theme': {},
- 'generated': '2026-08-07 19:19'}
+ 'generated': '2026-08-07 19:32'}
 
 MODULES = [{'key': 'rentals',
   'label': 'Rentals',
